30f260cdd5d0ce1e347841f654a47140c74fa7eb
Sitanshu Joshi <sitanshu@zymrinc.com>
2014-07-26 14:48:42 +0530

    Added constant file.

---
+++ Social Network/Constant.h
@@ -0,0 +1,15 @@
+//
+//  Constant.h
+//  Social Network
+//
+//  Created by Sitanshu Joshi on 7/26/14.
+//  Copyright (c) 2014 Sitanshu Joshi. All rights reserved.
+//
+
+#ifndef Social_Network_Constant_h
+#define Social_Network_Constant_h
+
+#define kBase_URL       @""
+
+
+#endif
